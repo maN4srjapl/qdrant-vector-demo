3726892da9f5a5f7c5c21a5994e813e5b2e65aa1
ManasRajpal <manasrajpal07@gmail.com>
2026-03-11 17:23:13 +0530

    accuracy_recovery with rescoring after quantization

---
+++ accuracy_recovery.py
@@ -0,0 +1,27 @@
+'''
+Oversampling + Rescoring
+- Oversampling: This technique involves generating synthetic data points to balance the dataset. It can help improve the performance of machine learning models by providing more examples for underrepresented classes.
+- Rescoring: After generating synthetic data points, you can use a rescoring mechanism to evaluate the relevance of the generated points to the original data. This can help filter out less relevant synthetic points and improve the overall accuracy of the model.
+
+'''
+
+from qdrant_client import QdrantClient, models
+import os
+
+client = QdrantClient(url=os.getenv("QDRANT_URL"))
+                      
+
+response = client.query_points(
+    collection_name="store",
+    query=[0.12] * 384,
+    limit=10,
+    search_params=models.SearchParams(
+        hnsw_ef=128, # Higher ef values can improve recall but may increase search time
+        quantization=models.QuantizationSearchParams(
+            ignore=False, # Consider quantized vectors during search
+            rescore=True,   # Enable original vectors-based rescoring
+            oversampling=3.0,  # Retrieve 3x candidates for rescoring
+        ),
+    ),
+    with_payload=True,
+)
