87651b3899b127ef6ed0a065eb2ec4397b4ed7a3
ManasRajpal <manasrajpal07@gmail.com>
2026-03-11 17:24:22 +0530

    adding expl for acc_Resc

--- accuracy_recovery.py
+++ accuracy_recovery.py
@@ -3,6 +3,11 @@ Oversampling + Rescoring
 - Oversampling: This technique involves generating synthetic data points to balance the dataset. It can help improve the performance of machine learning models by providing more examples for underrepresented classes.
 - Rescoring: After generating synthetic data points, you can use a rescoring mechanism to evaluate the relevance of the generated points to the original data. This can help filter out less relevant synthetic points and improve the overall accuracy of the model.
 
+If quantization is impacting performance in an application that requires high accuracy, 
+combining oversampling with rescoring is a great choice. However, if you need faster searches and can tolerate some loss in accuracy, 
+you might choose to use oversampling without rescoring, or adjust the oversampling factor to a lower value.
+
+
 '''
 
 from qdrant_client import QdrantClient, models
